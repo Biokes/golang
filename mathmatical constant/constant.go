5cc84fb3c00a8521c86b1965f7c3583107b8b3a1
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 19:57:02 +0100

    refactor maths constant

--- mathmatical constant/constant.go
+++ mathmatical constant/constant.go
@@ -1,29 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println(collectInput())
 }
+
 func collectInput() float64 {
 	number := 0
 	for number < 1 {
 		fmt.Println("Enter a number to get its maths value!!: ")
-		number, _ = fmt.Scanln(&number)
+		_, err := fmt.Scanln(&number)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+		if number < 1 {
+			fmt.Println("Please enter a number greater than or equal to 1.")
+		}
 	}
 	return getConstant(number)
 }
+
 func getConstant(number int) float64 {
 	total := 0.0
-	for count := 1; count < int(number); count++ {
-		total += 1.0 / factorial(float64(count))
+	for i := 0; i <= number; i++ {
+		total += 1.0 / factorial(float64(i))
 	}
 	return total
 }
-func factorial(number float64) float64 {
-	var value = 1.0
-	for count := 1; count < int(number); count++ {
-		value *= number
+
+func factorial(n float64) float64 {
+	if n == 0 {
+		return 1
 	}
-	return value
+	fact := 1.0
+	for i := 1; i <= int(n); i++ {
+		fact *= float64(i)
+	}
+	return fact
 }
