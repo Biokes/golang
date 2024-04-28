27764202a02da7177ac5a91f65463404abe1aa3d
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 18:13:47 +0100

    maths constant

--- mathmatical constant/constant.go
+++ mathmatical constant/constant.go
@@ -1,1 +1,34 @@
 package main
+
+import "fmt"
+
+func main() {
+	collectInput()
+}
+func collectInput() float64 {
+	var number int
+	fmt.Println("Enter a number to get its maths value: ")
+	number, _ = fmt.Scanln(&number)
+	for number <= 1 {
+		fmt.Println("Enter a number to get its maths value!!: ")
+		number, _ = fmt.Scanln(&number)
+	}
+	return getConstant(number)
+}
+func getConstant(number int) float64 {
+	value := 1
+	total := 0.0
+	for value <= number {
+		total += float64(value / factorial(value))
+		value++
+	}
+	return total
+}
+func factorial(number int) int {
+	value := 1
+	for number != 0 {
+		value *= number
+		number--
+	}
+	return number
+}
