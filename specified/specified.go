0fc3ac1d73af0de4c9ab84094e250e9364bb9710
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-29 00:20:08 +0100

    refactor maths constant

--- specified/specified.go
+++ specified/specified.go
@@ -4,18 +4,18 @@ import "fmt"
 
 func main() {
 	var number int
-	total := 0
 	value := 0
 	fmt.Println("Enter a number: ")
 	number, err := fmt.Scanln(&number)
 	if err != nil {
 		fmt.Println(err)
 	}
-	value = number
-	for total < value {
+	for number < value {
 		fmt.Println("Enter a number: ")
-		_, _ = fmt.Scanln(&number)
-		total += number
+		number, err = fmt.Scanln(&number)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	fmt.Println(number)
 }
