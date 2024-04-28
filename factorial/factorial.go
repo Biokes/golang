25bbb73d494a97a37edb063a3c8c7213e6bdbc0a
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 17:40:44 +0100

    refactor world Population Growth

---
+++ factorial/factorial.go
@@ -0,0 +1,5 @@
+package factorial
+
+func main() {
+
+}
--- world/worldPopulationGrowth.go
+++ world/worldPopulationGrowth.go
@@ -5,11 +5,14 @@ import "fmt"
 func main() {
 	year := 2022
 	population := 7951000000
+	populationIncrease := population
 	for i := 1; i <= 75; i++ {
-		fmt.Println("Year population")
-		fmt.Printf("%d %-2d\n", year, population)
+		fmt.Printf("Year population %s", "current increase")
+		fmt.Printf("/n%d %-2d %-2d", year, population-populationIncrease, population)
+		populationIncrease = population
 		year += 1
 		population += calculatePopulation(population)
+
 	}
 }
 func calculatePopulation(population int) int {
