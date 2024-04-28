012ed7ef0a9e220152ea240d256a820b17270f2b
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 22:51:06 +0100

    refactor maths constant

--- comparator/comparator.go
+++ comparator/comparator.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	number, _ := fmt.Println("enter  number: ")
+	number1, _ := fmt.Println("enter  number: ")
+	if number > number1 {
+		fmt.Println("0")
+	}
+	if number < number1 {
+		fmt.Println("1")
+	}
+	if number == number1 {
+		fmt.Println("-1")
+	}
 }
