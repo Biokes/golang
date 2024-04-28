aac2332fef7dc6da6b9e7630f3a5dcf6e6bd7032
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 17:45:39 +0100

    refactor world Population Growth

--- world/worldPopulationGrowth.go
+++ world/worldPopulationGrowth.go
@@ -7,8 +7,8 @@ func main() {
 	population := 7951000000
 	populationIncrease := population
 	for i := 1; i <= 75; i++ {
-		fmt.Printf("Year population %s", "current increase")
-		fmt.Printf("\n%d %-2d %-2d", year, population-populationIncrease, population)
+		fmt.Printf("%s %s %4s", "Year", "increase", "current increase")
+		fmt.Printf("\n%d %d %21d\n", year, population-populationIncrease, population)
 		populationIncrease = population
 		year += 1
 		population += calculatePopulation(population)
