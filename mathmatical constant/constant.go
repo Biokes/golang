7fe2a26a76fd05595cecc6e4326e9ba1523d7d53
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 20:10:35 +0100

    refactor maths constant

--- mathmatical constant/constant.go
+++ mathmatical constant/constant.go
@@ -9,20 +9,23 @@ func collectInput() float64 {
 	number := 0
 	for number < 1 {
 		fmt.Println("Enter a number to get its maths value!!: ")
-		number, _ = fmt.Scanln(&number)
+		_, err := fmt.Scanln(&number)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	return getConstant(number)
 }
 func getConstant(number int) float64 {
 	total := 0.0
-	for count := 1; count < number; count++ {
+	for count := 0; count <= number; count++ {
 		total += 1.0 / factorial(float64(count))
 	}
 	return total
 }
 func factorial(number float64) float64 {
 	var value = 1.0
-	for count := 1; count < int(number); count++ {
+	for count := 1; count <= int(number); count++ {
 		value *= float64(count)
 	}
 	return value
