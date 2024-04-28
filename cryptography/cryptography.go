b0a3ccd20661ad4403519458fd98d14262b1316f
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 17:06:47 +0100

    decrypt and enryption

--- cryptography/cryptography.go
+++ cryptography/cryptography.go
@@ -5,11 +5,15 @@ import (
 	"strconv"
 )
 
+func wrapToInteger(value string) int {
+	output, _ := strconv.ParseInt(value, 10, 32)
+	return int(output)
+}
 func encryptNumber(numberGiven string) string {
-	index1, _ := strconv.ParseInt(numberGiven[0], 10, 64)
-	index2 := numberGiven[1]
-	index3 := numberGiven[2]
-	index4 := numberGiven[3]
+	index1 := (wrapToInteger(string(numberGiven[0])) + 7) % 10
+	index2 := (wrapToInteger(string(numberGiven[1])) + 7) % 10
+	index3 := (wrapToInteger(string(numberGiven[2])) + 7) % 10
+	index4 := (wrapToInteger(string(numberGiven[3])) + 7) % 10
 	return fmt.Sprintf("%d%d%d%d", index3, index4, index1, index2)
 }
 func validate(value string) string {
@@ -25,13 +29,23 @@ func getInput() string {
 	_, _ = fmt.Scanln(&input)
 	return validate(input)
 }
-func decrypt(value string) string {
-
-	return ""
+func decrypt(input string) int {
+	return (wrapToInteger(input) + 3) % 10
+}
+func decryptGiven(value string) string {
+	index1 := decrypt(string(value[0]))
+	index2 := decrypt(string(value[1]))
+	index3 := decrypt(string(value[2]))
+	index4 := decrypt(string(value[3]))
+	return fmt.Sprintf("%d%d%d%d", index3, index4, index1, index2)
+}
+func encrypt() string {
+	input := getInput()
+	validate(input)
+	return encryptNumber(input)
 }
 func main() {
-	number := getInput()
-	str := encryptNumber(number)
-	value := decrypt(str)
-	fmt.Println(fmt.Sprintf("Encrypt: %s\nDecrypt : %s ", number, value))
+	input := encrypt()
+	value := decryptGiven(input)
+	fmt.Println(fmt.Sprintf("Encrypt: %s\nDecrypt : %s ", input, value))
 }
