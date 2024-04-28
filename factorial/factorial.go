b75dda1d9e66fb1badf9ea7309281c472fc64ab0
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-28 17:56:03 +0100

    factorial

--- factorial/factorial.go
+++ factorial/factorial.go
@@ -1,5 +1,20 @@
-package factorial
+package main
+
+import "fmt"
 
 func main() {
-
+	value := 0
+	for value < 0 {
+		fmt.Print("Enter your number(greater than 0) to get the factorial: ")
+		_, _ = fmt.Scanln(&value)
+	}
+	fmt.Printf("Factorial of %d is %d\n", value, getFactorial(value))
+}
+func getFactorial(value int) int {
+	number := 1
+	for value != 0 {
+		number *= value
+		value--
+	}
+	return number
 }
