9fa9c06c6a0f9654788a588dd399eb812e38b1b9
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-29 06:28:08 +0100

    triangle2

--- triangle2/triangle2.go
+++ triangle2/triangle2.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	for count := 0; count < 10; count++ {
 		for index := 0; index < count; index++ {
-			fmt.Println(" ")
+			fmt.Print(" ")
 		}
 		for couter := 0; couter < 10-count; couter++ {
-			fmt.Println("#")
+			fmt.Print("#")
 		}
 		fmt.Println()
 	}
