ec915ba68ae383e4b2fa108a8b8dd6e61694aba6
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-29 01:49:00 +0100

    palindrome

---
+++ palindrome/palindrome.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func collectInput() string {
+	var input string
+	fmt.Print("Enter a five digit number: ")
+	_, _ = fmt.Scanln(&input)
+	for len(input) != 5 {
+		collectInput()
+	}
+	return input
+}
+func isPalindrome(value string) bool {
+	palindrome := fmt.Sprintf("%s%s%s%s%s", string(value[4]), string(value[3]),
+		string(value[2]), string(value[1]), string(value[0]))
+	return value == palindrome
+}
+func main() {
+	input := collectInput()
+	fmt.Println(isPalindrome(input))
+}
